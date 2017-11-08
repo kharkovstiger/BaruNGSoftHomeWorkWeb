bd038278ecb9665575ba1e14b71c0a179da1748d
kharkovstiger <kharkovs.tiger@gmail.com>
2017-11-08 15:57:41 +0300

    number of orders

--- js/main.js
+++ js/main.js
@@ -7,6 +7,17 @@ app.controller('mainCtrl', ['$scope', '$http', 'customerId', '$location', functi
         function (response) {
             // console.log(response);
             $scope.customers=response.data;
+            var list=[];
+            for(var i=0;i<$scope.customers.length;i++){
+                list.push($scope.customers[i].id);
+            }
+            $http.post(baseURL+"/order/getNumberOfOrders",list).then(
+                function (response) {
+                    for(var i=0;i<$scope.customers.length;i++){
+                        $scope.customers[i].numberOfOrders=response.data[$scope.customers[i].id];
+                    }
+                }
+            );
         },
         function (response) {
             console.log(response.status+", "+response.data);
